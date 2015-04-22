3ce0648e42a19f4f476cb100a9233bdc24ab0fd2
psaylor <psaylor42@gmail.com>
2015-04-21 22:42:19 -0400

    Implemented recorder module
    Added some more server components under devDependencies for testing the
    functionality of recorder

--- test/app.js
+++ test/app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var path = require('path');
 var http = require('http');
-
 var rr = require('ractive-render');
+var socketIO = require('socket.io');
+var ss = require('socket.io-stream');
+
 var app = express();
 
 app.engine('html', rr.renderFile);
@@ -21,4 +23,17 @@ var port = 3001;
 app.set('port', port);
 
 var server = http.createServer(app);
+var io = socketIO(server);
+io.on('connection', function (socket) {
+    console.log('Connected to client socket');
+    ss(socket).on('audioStream', function (stream, data) {
+        console.log('Receiving stream audio with data', data);
+        stream.on('data', function (d) {
+            console.log('Got data', typeof(d), d.length);
+        });
+        stream.on('end', function (){
+            console.log('End of audio stream');
+        });
+    });
+});
 server.listen(port);
